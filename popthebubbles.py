56bb2507dfc56e96d28a90f78d0d21cf8a86a867
Mac User <macuser@MacintoshUsers-MacBook-2.local>
2017-06-29 15:27:14 -0400

    Made bubbles move.

--- popthebubbles.py
+++ popthebubbles.py
@@ -1,45 +1,73 @@
 import pygame
+import time
+import random
 
-class Background(pygame.sprite.Sprite):
-    def __init__(self, image_file, location):
+
+class Bubble(pygame.sprite.Sprite):
+    def __init__(self, image_file, x, y, speed):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(image_file)
-        self.image = pygame.transform.scale(self.image, (1280, 800))
+        # self.speed_x = speed
+        self.speed_y = speed
+        self.image = pygame.image.load(image_file).convert_alpha()
+        self.image = pygame.transform.scale(self.image, (75, 75))
+        # self.radius = radius
         self.rect = self.image.get_rect()
-        self.rect.left, self.rect.top = location
+        self.rect.x = x
+        self.rect.y = y
+        # self.pressed = False
+    
+    def update(self):
+        if self.rect.y <= 750:
+            self.rect.y -= self.speed_y
+        mouse_pos = pygame.mouse.get_pos()
+        mouse_clicked = pygame.mouse.get_pressed()[0]
+        if self.rect.collidepoint(*mouse_pos) and mouse_clicked:
+            self.kill()
 
-class 
 def main():
-    width = 800
-    height = 800
+    width = 700
+    height = 700
     midnight_blue = (25, 25, 112)
 
     pygame.init()
     screen = pygame.display.set_mode((width, height))
-    BG = Background("underwater.jpg", [0,0])
+   #  background = pygame.image.load("underwater.jpg").convert_alpha()
+    # background = pygame.transform.scale(background, (1280, 800))
     pygame.display.set_caption("Pop the Bubbles!")
     clock = pygame.time.Clock()
 
+    bubble_list = pygame.sprite.RenderPlain()
+    # bubble = Bubble("bubble.png", random.randrange(700), 700, 5)
+    # bubble_list.append(bubble)
+
     # Game initialization
-    bubble = pygame.image.load("bubble.png")
     stop_game = False
+    
     while not stop_game:
         for event in pygame.event.get():
-
-            # Event handling
+            for i in range(50):
+                # bubble.update()
+                bubble = Bubble("bubble.png", random.randrange(10, 690), 710, 5)
+                bubble_list.add(bubble)
+                # Event handling
+            # if event.type == pygame.MOUSEBUTTONDOWN:
+            #      bubble.update()
 
             if event.type == pygame.QUIT:
                 stop_game = True
+        for bubble in bubble_list:
+            bubble.update()
 
 
         # Game logic
 
         # Draw background
         screen.fill(midnight_blue)
-        screen.blit(BG.image, BG.rect)
+        # screen.blit(background, (0,0))
         
 
         # Game display
+        screen.blit(bubble.image, (bubble.rect))
         
         pygame.display.update()
         clock.tick(60)
