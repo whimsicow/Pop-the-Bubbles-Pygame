7c12d90a8f09515f1d042cebf18e0fc7ad6c857b
sabbey37 <sabbey37@users.noreply.github.com>
2017-06-29 21:42:18 -0400

    Add files via upload
    Added bubblepop image.

--- popthebubbles.py
+++ popthebubbles.py
@@ -1,27 +1,55 @@
 import pygame
-import time
+import pygame,sys
+from pygame.locals import *
 import random
 
+def game_intro():
+
+    intro = True
+
+    while intro:
+        for event in pygame.event.get():
+            print(event)
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+                
+        gameDisplay.fill(white)
+        largeText = pygame.font.Font('freesansbold.ttf',115)
+        TextSurf, TextRect = text_objects("Pop the Bubbles!", largeText)
+        TextRect.center = ((display_width/2),(display_height/2))
+        smallText = pygame.font.Font("freesansbold.ttt",75)
+        TextSurf, TextRect = text_objects("Click on the bubbles to pop them before they fill your screen.", smallText)
+        gameDisplay.blit(TextSurf, TextRect)
+        pygame.display.update()
+        clock.tick(15)
+
 def main():
+    pygame.init()
+
     width = 700
     height = 700
     midnight_blue = (25, 25, 112)
-    number_of_bubbles = 30
 
-    pygame.init()
     screen = pygame.display.set_mode((width, height))
     background = pygame.image.load("underwater.jpg").convert_alpha()
     background = pygame.transform.scale(background, (1280, 800))
     pygame.display.set_caption("Pop the Bubbles!")
     clock = pygame.time.Clock()
 
+    font = pygame.font.Font(None, 36)
+    score = 0
+
     class Bubble_Creation(object):
         def __init__(self):
             self.bubble_list = []
+            self.number_of_bubbles = 10
 
-        def create(self, number_of_bubbles):
-            for i in range(number_of_bubbles):
+        def create(self):
+            for i in range(self.number_of_bubbles):
                 self.bubble_list.append(Bubble("bubble.png", random.randrange(10, 690), 710, 3))
+                self.number_of_bubbles += 1
+
 
     class Bubble(pygame.sprite.Sprite):
         def __init__(self, image_file, x, y, speed):
@@ -33,25 +61,27 @@ def main():
             self.rect.x = x
             self.rect.y = y
         
-        def update(self):
+        def update(self, score):
             screen.blit(self.image, (self.rect))
-            if self.rect.y == 10:
-                self.rect.y = 10
+            if self.rect.y <= 5:
+                self.rect.y = 5
 
             elif self.rect.y <= 750:
                 self.rect.y -= self.speed
                 mouse_pos = pygame.mouse.get_pos()
-                mouse_clicked = pygame.mouse.get_pressed()[0]
-                if self.rect.collidepoint(*mouse_pos) and mouse_clicked:
+                mouse_clicked = pygame.mouse.get_pressed()
+                if self.rect.collidepoint(*mouse_pos) and mouse_clicked[0]:
                     pop = pygame.image.load("bubblepop.png").convert_alpha()
                     pop = pygame.transform.scale(pop, (135, 125))
                     self.image = pop
                     screen.blit(pop, (mouse_pos))
-                    self.rect.y = -200
-                    self.rect.x = -200
+                    self.kill()
+                    pygame.time.delay(10)
+                    self.rect.y = 1000
+                    self.rect.x = 1000
 
     all_bubbles = Bubble_Creation()
-    all_bubbles.create(number_of_bubbles) 
+    all_bubbles.create() 
 
     # Game initialization
     stop_game = False
@@ -61,18 +91,23 @@ def main():
         for event in pygame.event.get():
                 # Event handling
             # if event.type == pygame.MOUSEBUTTONDOWN:
-            #      bubble.update()
+            #     all_bubbles = Bubble_Creation()
+            #     all_bubbles.create() 
 
             if event.type == pygame.QUIT:
                 stop_game = True
 
         screen.blit(background, (0,0))
-
-        for i in range(len(all_bubbles.bubble_list)):
-            all_bubbles.bubble_list[i].update()
-
-
+        
         # Game logic
+
+        for i in range(len(all_bubbles.bubble_list)): 
+            all_bubbles.bubble_list[i].update(score)
+            # all_bubbles = Bubble_Creation()
+            # all_bubbles.create()
+
+
+        
 
         # Draw background
         # screen.fill(midnight_blue)
