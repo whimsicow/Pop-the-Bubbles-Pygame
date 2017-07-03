4c086d7bf880f4436da1fb45c81031dd1d4ac13c
Mac User <sabbey37@gmail.com>
2017-07-03 16:19:59 -0400

    Added comments explaining code

--- popthebubbles.py
+++ popthebubbles.py
@@ -1,6 +1,7 @@
 import pygame, sys, random, time
 from pygame.locals import *
 
+# Sets up specifications for screen, loads background image
 width = 700
 height = 700
 screen = pygame.display.set_mode((width, height))
@@ -9,11 +10,13 @@ background = pygame.transform.scale(background, (1280, 800))
 pygame.display.set_caption("Pop the Bubbles!")
 clock = pygame.time.Clock()
 
+# game intro function
 def game_intro(background):
     font = pygame.font.Font(None, 100)
     smallfont = pygame.font.Font(None, 25)
     intromax_bubbles = 15
     
+    # Specifications for bubbles in intro screen
     class IntroBubble(pygame.sprite.Sprite):
         def __init__(self, image_file, x, y, speed):
             pygame.sprite.Sprite.__init__(self, self.containers)
@@ -26,30 +29,37 @@ def game_intro(background):
         
         def update(self):
             screen.blit(self.image, (self.rect))
+            # Removes bubbles from all groups once they reach the top of the screen
             if self.rect.y <= 0:
                 self.kill()
 
-            elif self.rect.y <= 710 and self.speed != 0:
+            # Allows bubbles to move from the bottom to the top of the screen
+            elif self.rect.y <= 710:
                 self.rect.y -= self.speed
                 
     intro = True
+    # Assigns all bubbles in list to sprite group
     introbubble_list = pygame.sprite.Group()
     IntroBubble.containers = introbubble_list
     
     while intro:
         for event in pygame.event.get():
+            # Exits game when screen is closed
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
+            # Exits intro screen when key is pressed
             elif event.type == pygame.KEYDOWN:
                 intro = False
-
+        # Prints underwater background image
         screen.blit(background, (0,0))
 
+        # Prints more bubbles to screen when there are less than specified # of max bubbles on screen
         if len(introbubble_list) < intromax_bubbles:
-                for i in range(random.randint(1,3)):
-                    IntroBubble("bubble.png", random.randint(0, 680), 710, random.randint(1,4))
-                
+            for i in range(random.randint(1,3)):
+                IntroBubble("bubble.png", random.randint(0, 680), 710, random.randint(1,4))
+        
+        # Renders text and subtext        
         text = font.render("Pop the Bubbles", True, (255, 255, 255), None)
         text_rect = text.get_rect()
         subtext = smallfont.render("Click on the bubbles to pop them before they reach the top of your screen.", True, (255, 255, 255), None)
@@ -63,6 +73,7 @@ def game_intro(background):
         instructions_x = screen.get_width() / 2 - instructions_rect.width / 2
         instructions_y = 650
 
+        # Prints text and bubbles
         introbubble_list.update()
         screen.blit(text, [text_x, text_y])
         screen.blit(subtext, [subtext_x, subtext_y])
@@ -72,6 +83,7 @@ def game_intro(background):
 
 def main(background):
     pygame.init()
+    # Sets up variables and font specifications
     bubble_time = 0
     font = pygame.font.Font(None, 100)
     smallfont = pygame.font.Font(None, 30)
@@ -92,7 +104,8 @@ def main(background):
                 last_time_ms = int(round(time.time() * 1000))
             else:
                 pass
-
+    
+    # Main game Bubble specifications
     class Bubble(pygame.sprite.Sprite):
         def __init__(self, image_file, x, y, speed):
             pygame.sprite.Sprite.__init__(self, self.containers)
@@ -106,55 +119,61 @@ def main(background):
         
         def update(self, score):
             screen.blit(self.image, (self.rect))
+            # Stops bubbles once they reach the top of the screen, sets speed to 0
             if self.rect.y <= 0:
                 self.rect.y = 0
                 self.speed = 0
-
+            # Indicates game is over once stopped bubbles reach the bottom of the screen
             elif self.speed == 0 and self.rect.bottom >= 690:
                 game_over_list.append(True)
-
+            # Allows bubbles to float from the bottom to the top of the screen based on specified speed
             elif self.rect.y <= 710 and self.speed != 0:
                 self.rect.y -= self.speed
+                # Specifies if mouse has been clicked, gets position of mouse
                 mouse_pos = pygame.mouse.get_pos()
                 mouse_clicked = pygame.mouse.get_pressed()
-                
+                # Collision detection: pops bubbles if they have been clicked
                 if self.rect.collidepoint(*mouse_pos) and mouse_clicked[0]:
                     pop = pygame.image.load("bubblepop.png").convert_alpha()
                     pop = pygame.transform.scale(pop, (135, 125))
+                    # Shows pop image once bubbles are clickec
                     self.image = pop
                     screen.blit(pop, (mouse_pos))
                     score.append(15)
+                    # Removes clicked bubble from all groups
                     self.kill()
-
+        
+        # Stops bubbles once they collide with other stopped bubbles
         def collide(self, bubble_list):
             collision = pygame.sprite.spritecollide(self, bubble_list, False, pygame.sprite.collide_circle)
             for x in collision:
                 if x.speed == 0:
                     self.speed = 0
     
+    # Adds bubbles to sprite group
     allSprites = pygame.sprite.Group()
     bubble_list = pygame.sprite.Group()
     Bubble.containers = allSprites, bubble_list
-    # ScoreText.containers = allSprites
 
     # Game initialization
     close_window = False
+    # Plays intro
     game_intro(background)
 
     while not close_window:
         for event in pygame.event.get():
-            # Event handling
+            # Ends game if user exits screen
             if event.type == pygame.QUIT:
                 close_window = True
-            
+            # Indicates game is over once score of 900 reached
             elif sum(score) >= 900:
                 game_over = True
-        
+        # Indicates game is over once stopped bubbles reach bottom of screen
         for x in game_over_list:
             if x == True:
                 game_over = True
             
-        # Draw background
+        # Draw background and score counter
         screen.blit(background, (0,0))
         scoretext = smallfont.render("Score: %d" % sum(score), True, (255, 255, 255), None)
         scoretext_rect = scoretext.get_rect()
@@ -164,12 +183,13 @@ def main(background):
         if not game_over:
             for bubble in bubble_list:
                 bubble.update(score)
-
+            # Iterates through bubble list, checks for collisions, removes bubble and adds it back to ensure it does not detect the bubble colliding with itself
             for bubble in bubble_list:
                 bubble_list.remove(bubble)
                 bubble.collide(bubble_list)
                 bubble_list.add(bubble)
-
+            
+            # Generates 1-3 bubbles with random speed and x poistion, slight delay due to bubble_cooldown/bubble_delay variables
             bubble_cooldown -= 1
             if len(bubble_list) < max_bubbles and bubble_cooldown <= 0:
                 for i in range(random.randint(1,3)):
@@ -178,10 +198,10 @@ def main(background):
 
         # Reload game display
         elif game_over:
-            # If game over and score over 900, draw you win.
+            # Stops all bubbles once game is over
             for bubble in bubble_list:
                 bubble.speed = 0
-
+            # If game over and score over 900, draw you win
             if sum(score) >= 900:
                 text = font.render("You win!", True, (255, 255, 255), None)
                 text_rect = text.get_rect()
